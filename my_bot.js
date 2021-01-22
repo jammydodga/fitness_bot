12534e3f9706a2d0573e0148000dc9ce4b53bb3d
jammydodga <57770102+jammydodga@users.noreply.github.com>
2021-01-22 16:16:03 +0000

    Update my_bot.js

--- my_bot.js
+++ my_bot.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 require('dotenv').config()
 const client = new Discord.Client()
 const user_list = ["Will", "Mason", "Lee", "Tom", "Ally", "James"]
-const activitylist = ["push-ups", "pull-ups", "dips", "sit-ups", "chin-ups", "star-jumps"]
+const activitylist = ["push-ups", "pull-ups", "dips", "sit-ups", "chin-ups", "star-jumps", "squats"]
 const token = process.env.TOKEN
 
 client.on('ready', () => {
@@ -15,23 +15,23 @@ client.on('ready', () => {
 })
 
 client.on('message', msg => {
-    if (msg.content == '-randworkout') {
+    if (msg.content == '!randworkout') {
         let rand1 = Math.floor((Math.random() * 100) + 1);
         let rand2 = Math.floor((Math.random() * 6));
-        let rand3 = Math.floor((Math.random() * 6));
+        let rand3 = Math.floor((Math.random() * 7));
 
       msg.reply(user_list[rand2] + " has to do " + rand1 + " " + activitylist[rand3])
     }
   });
 
   client.on('message', msg => {
-    if (msg.content == '-help') {
+    if (msg.content == '!help') {
       msg.reply("This bot helps the boys make gains by choosing a random number that represents one member of the group and then another random integer to decide how many and another random integer to describe the activity. Try -workout for random or -meworkout for personal")
     }
   });
 
   client.on('message', msg => {
-    if (msg.content == '-meworkout') {
+    if (msg.content == '!meworkout') {
         let rand1 = Math.floor((Math.random() * 100) + 1);
         let rand3 = Math.floor((Math.random() * 6));
 
