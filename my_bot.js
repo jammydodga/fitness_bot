75ebd3b711e58b202cd7df224deb9432f2046fab
jammydodga <57770102+jammydodga@users.noreply.github.com>
2021-01-23 10:39:00 +0000

    Bug fix + refinement

--- my_bot.js
+++ my_bot.js
@@ -1,5 +1,6 @@
 const { randomInt } = require('crypto')
 const Discord = require('discord.js')
+const sqlite =  require('sqlite3').verbose();
 require('dotenv').config()
 const client = new Discord.Client()
 const user_list = ["Will", "Mason", "Lee", "Tom", "Ally", "James"]
@@ -8,7 +9,7 @@ const token = process.env.TOKEN
 
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
-
+    let db = new sqlite.Database('./fitness_boys.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE)
     client.user.setActivity("Gains-Simulator 2021")
 
     
@@ -21,22 +22,23 @@ client.on('message', msg => {
         let rand3 = Math.floor((Math.random() * 7));
 
       msg.reply(user_list[rand2] + " has to do " + rand1 + " " + activitylist[rand3])
-    }
-  });
+      let db = new sqlite.Database('./fitness_boys.db', sqlite.OPEN_READWRITE)
 
-  client.on('message', msg => {
+      if (msg == ".getdata") {
+        let query = 'SELECT * FROM user WHERE '
+      }
+      db.run('')
+    }
     if (msg.content == '!help') {
       msg.reply("This bot helps the boys make gains by choosing a random number that represents one member of the group and then another random integer to decide how many and another random integer to describe the activity. Try !workout for random or !meworkout for personal")
     }
-  });
-
-  client.on('message', msg => {
+    
     if (msg.content == '!meworkout') {
-        let rand1 = Math.floor((Math.random() * 100) + 1);
-        let rand3 = Math.floor((Math.random() * 6));
+      let rand1 = Math.floor((Math.random() * 100) + 1);
+      let rand3 = Math.floor((Math.random() * 6));
 
-      msg.reply("you have to do " + rand1 + " " + activitylist[rand3])
-    }
-  });
+    msg.reply("you have to do " + rand1 + " " + activitylist[rand3])
+  }
+});
 
 client.login(process.env.BOT_TOKEN)
