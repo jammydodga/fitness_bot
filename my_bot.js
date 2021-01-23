faf4da8f03acd642915942279870710cac58aeba
jammydodga <57770102+jammydodga@users.noreply.github.com>
2021-01-23 10:49:21 +0000

    Update my_bot.js

--- my_bot.js
+++ my_bot.js
@@ -16,6 +16,8 @@ client.on('ready', () => {
 })
 
 client.on('message', msg => {
+  let userid = message.author.id
+  let uname = message.author.tag;
     if (msg.content == '!workout') {
         let rand1 = Math.floor((Math.random() * 100) + 1);
         let rand2 = Math.floor((Math.random() * 6));
@@ -25,14 +27,32 @@ client.on('message', msg => {
       let db = new sqlite.Database('./fitness_boys.db', sqlite.OPEN_READWRITE)
 
       if (msg == ".getdata") {
-        let query = 'SELECT * FROM user WHERE '
+        let query = 'SELECT * FROM user WHERE userid = ?'
+        db.get(query, [userid], (err, row) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          if (row === undefined){
+            let insertdata = db.prepare('INSERT INTO user VALUES (?, ?)')
+            insertdata.run(userid, uname)
+            db.close();
+            return;
+          }
+          else {
+            let userid2 = row.userid;
+            let name = row.name;
+            console.log(name)
+            
+          }
+        })
       }
       db.run('')
     }
     if (msg.content == '!help') {
       msg.reply("This bot helps the boys make gains by choosing a random number that represents one member of the group and then another random integer to decide how many and another random integer to describe the activity. Try !workout for random or !meworkout for personal")
     }
-    
+
     if (msg.content == '!meworkout') {
       let rand1 = Math.floor((Math.random() * 100) + 1);
       let rand3 = Math.floor((Math.random() * 6));
